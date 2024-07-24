7b288e60b1a1cd057ae15a8cafde5c14ff82d3e6
islomkhodja <islomkhodjadev@gmail.com>
2024-07-24 21:35:03 +0500

    With new buttons widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:zoom/utils/colors.dart";
+import "package:zoom/widgets/home_meeting_button.dart";
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -27,8 +28,31 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(children: [
         Row(
-          children: [],
-        )
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            HomeMeetingButton(
+                onpressed: () {}, icon: Icons.videocam, text: "New Meeting"),
+            HomeMeetingButton(
+                onpressed: () {},
+                icon: Icons.add_box_rounded,
+                text: "Join Meeting"),
+            HomeMeetingButton(
+                onpressed: () {},
+                icon: Icons.calendar_today,
+                text: "Schedule meeting"),
+            HomeMeetingButton(
+                onpressed: () {},
+                icon: Icons.arrow_upward_rounded,
+                text: "Share screen")
+          ],
+        ),
+        const Expanded(
+            child: Center(
+          child: Text(
+            "Create/Join Meetings with just a click!",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+          ),
+        ))
       ]),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: footerColor,
